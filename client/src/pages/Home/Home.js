42f472b0b58e3495ad69265125ba72d35785f8b9
Lucas-bit <lucasbertaudgonzalez@gmail.com>
2020-04-20 18:26:26 -0400

    login page is mobile with the exception of tablet 768px width, deleted succulent component and incorporated teh photo directly in the html

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import SucculentPhoto from '../../components/SucculentPhoto'
 import Login from '../../components/Login'
 import { Col, Row, Container } from '../../components/Grid'
 
 function Home(){
     return(
     <div>
-        <Row>
-            <SucculentPhoto/>
             <Login/>
-        </Row>
     </div>
     )
 }
--- client/src/pages/MyGarden/myGarden.js
+++ client/src/pages/MyGarden/myGarden.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { Col, Container, Row } from '../../components/Grid'
-import Fence from '../../components/Fence/fence'
 import Cards from '../../components/GardenCards'
-import NavBar from '../../components/NavBar'
-import Title from '../../components/Title/index'
 import Hamburger from '../../components/Hamburger/index'
 
 function MyGarden(){
