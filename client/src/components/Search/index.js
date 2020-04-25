4d7349b9da9244f6396e889c7d739fedda57053c
Lucas-bit <lucasbertaudgonzalez@gmail.com>
2020-04-25 10:19:30 -0400

    adding changes from yesterday, re-sized the search page, and attempted to add statefullness to the add plant button

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -5,7 +5,7 @@ import './style.css'
 
 
 
-const plantArr = [
+let plantArr = [
   {
   name:'tree'  
   }
@@ -53,19 +53,8 @@ function SearchResults(props) {
                               data-id={item.id}
                               data-name={item.common_name}
                               data-scientific={item.scientific_name}
-                              onClick={(event)=>{
-                                console.log(event.target.dataset)
-                                  fetch("http://localhost:3001/api", {
-                                    crossOrigin: true,
-                                    origin: "http://localhost:3000/search",
-                                    method:"POST",
-                                    body: event.target.dataset
-                                      })
-                                  .then(response=>{console.log(response)})
-                                  .then(plantArr.push(event.target.dataset))
-                                  .then(console.log(plantArr))
-                              }
-                                    }
+                              onClick={props.handlePlantChange.bind(this,item)}
+                           
                                      >Add {item.common_name} to my garden!
                                 </button>
                           </div>
--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -5,72 +5,91 @@ import SearchResults from '../../components/Search/index';
 import { Row } from '../../components/Grid';
 import Hamburger from '../../components/Hamburger/index';
 
-    function Search() {
-        const [search, setSearch] = useState("");
-        const [results, setResults] = useState([]);
-        const [term, setTerm] = useState("")
-        const [error, setError] = useState("");
+function Search() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const [term, setTerm] = useState("");
+  const [error, setError] = useState("");
+  const [plants, setPlants] = useState([]); //if to push back to the app.js and pass the actual plant object to the garden
+
+  const handlePlantChange = (plant, event) => {
+    const newPlants = [...plants, plant]  //taking original planst and binding them together and saving them to a new plant instance of plants 
+    setPlants(newPlants)
+    console.log(event.target.dataset)
+    fetch("http://localhost:3001/api", {
+      crossOrigin: true,
+      origin: "http://localhost:3000/search",
+      method: "POST",
+      body: plant
+    })
+      .then(response => { 
+        console.log(response) 
+        console.log(plants)
+      })
       
-        // When the component mounts, update the title to be Wikipedia Searcher
-        useEffect(() => {
+  };
+
+  // When the component mounts, update the title to be Wikipedia Searcher
+  useEffect(() => {
 
 
-          if (!term) {
-            return;
-          }
-                      
-          setSearch("")
-          console.log(search)
-          const key = "dFJuTGR0eWxpTTR5N2xXVTczWlMvZz09"
-          const APIurl = "https://cors-anywhere-guanaco.herokuapp.com/https://trefle.io/api/species/?q="+term+"&token="+key
-          
-          fetch(APIurl,{crossOrigin: true, origin: "http://localhost:3000"})
-            .then(res => {
-              res.json()
-                .then(resp=>{
-                  if (resp.length === 0) {
-                  throw new Error("No results found.");
-                  }
-                  if (resp.status === "error") {
-                  throw new Error(res.data.message);
-                  }
-                  let plantArr = resp
-                  setResults(plantArr)
-                  
-              }).catch(err=>{throw err})
-        }, [results])
-        .catch(err => setError(err));
+    if (!term) {
+      return;
+    }
 
-        setTerm("")
+    setSearch("")
+    console.log(search)
+    const key = "dFJuTGR0eWxpTTR5N2xXVTczWlMvZz09"
+    const APIurl = "https://cors-anywhere-guanaco.herokuapp.com/https://trefle.io/api/species/?q=" + term + "&token=" + key
 
-        console.log(term)
-        console.log(search)
+    fetch(APIurl, { crossOrigin: true, origin: "http://localhost:3000" })
+      .then(res => {
+        res.json()
+          .then(resp => {
+            if (resp.length === 0) {
+              throw new Error("No results found.");
+            }
+            if (resp.status === "error") {
+              throw new Error(res.data.message);
+            }
+            let plantArr = resp
+            setResults(plantArr)
 
-        });
-      
-        const clearForm = () =>{
-          setSearch("")
-        }
-        const handleInputChange = event => {
-            setSearch(event.target.value); 
-        };
+          }).catch(err => { throw err })
+      }, [results])
+      .catch(err => setError(err));
 
-        const handleFormSubmit = event => {
-          event.preventDefault();
-          setTerm(search)
-        };
+    setTerm("")
+
+    console.log(term)
+    console.log(search)
+
+  });
+
+  const clearForm = () => {
+    setSearch("")
+  }
+  const handleInputChange = event => {
+    setSearch(event.target.value);
+  };
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+    setTerm(search)
+  };
 
 
-        return (
-          <div>
-            <Hamburger/>
-              <Searchform
-                handleFormSubmit={handleFormSubmit}
-                handleInputChange={handleInputChange}
-                term={search}
-                />
-              <SearchResults results={results} />
-          </div>
-        )};
+  return (
+    <div>
+      <Hamburger />
+      <Searchform
+        handleFormSubmit={handleFormSubmit}
+        handleInputChange={handleInputChange}
+        term={search}
+      />
+      <SearchResults results={results} handlePlantChange={handlePlantChange} />
+    </div>
+  )
+};
 
 export default Search;
