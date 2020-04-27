6c9d531b351c8d5bdbfbf6ad68fd109d95329c74
Joey Register <josephregister1258@gmail.com>
2020-04-27 18:39:57 -0400

    added sample client fldr

---
+++ client/src/components/GardenCards/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './style.css'
+import Logo from '../../images/Garden-Guru-Transparent.png'
+
+
+function myServices(props){
+    
+    return (
+        <React.Fragment>
+            <div className="bigContainer">
+                <h1 className = 'myGarden-title'><span><img className ="myGarden-logo" src={Logo}/></span>MY GARDEN<span><img className ="myGarden-logo" src={Logo}/></span></h1>
+                    <div id="services" className="services-area section-padding">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="section-header">
+                                        <p className="line1"></p>
+                                        <p className="line2"></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                {props.plants.map(({ _id, name, type, height }) => {
+                                    return (
+                                        <div key={_id} className="col-xs-12 col-sm-6 col-md-4">
+                                            <div className="single-services text-center wow fadeInDown" data-wow-delay="0.2s">
+                                                <div className="services-icon">
+                                                    <i className="fa fa-users"></i>
+                                                </div>
+                                                <div className="services-content">
+                                                    <h3>{ name }</h3> 
+                                                </div>
+                                            </div>
+                                        </div>
+                                            )})}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+    )}
+
+export default myServices;
